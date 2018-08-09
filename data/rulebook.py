970e7fd276a16112d99d480c56d255ffaa7e9b93
shpaackle <shpaackle@gmail.com>
2018-08-09 18:29:59 -0400

    initial commit

---
+++ data/rulebook.py
@@ -0,0 +1,446 @@
+SIMPLE_MELEE = [
+    {
+        "name": "Club",
+        "price": "0 sp",
+        "damage": "1d6 B",
+        "bulk": "1",
+        "hands": 1,
+        "group": "Club",
+        "traits": ["Thrown 10 ft."]
+    },
+    {
+        "name": "Dagger",
+        "price": "2 sp",
+        "damage": "1d4 P",
+        "bulk": "L",
+        "hands": 1,
+        "group": "Knife",
+        "traits": ["Agile", "finesse", "thrown 10 ft.", "versatile S"]
+    },
+    {
+        "name": "Fist",
+        "price": "-",
+        "damage": "1d4 B",
+        "bulk": "-",
+        "hands": 1,
+        "group": "Brawling",
+        "traits": ["Agile", "finesse", "nonlethal", "unarmed"]
+    },
+    {
+        "name": "Gauntlet",
+        "price": "2 sp",
+        "damage": "1d4 B",
+        "bulk": "L",
+        "hands": 1,
+        "group": "Brawling",
+        "traits": ["Agile", "free-hand"]
+    },
+    {
+        "name": "Light mace",
+        "price": "4 sp",
+        "damage": "1d4 B",
+        "bulk": "L",
+        "hands": 1,
+        "group": "Club",
+        "traits": ["Agile", "finesse", "shove"]
+    },
+    {
+        "name": "Longspear",
+        "price": "5 sp",
+        "damage": "1d8 P",
+        "bulk": "2",
+        "hands": 2,
+        "group": "Spear",
+        "traits": ["Reach"]
+    },
+    {
+        "name": "Mace",
+        "price": "12 sp",
+        "damage": "1d6 B",
+        "bulk": "1",
+        "hands": 1,
+        "group": "Club",
+        "traits": ["Shove"]
+    },
+    {
+        "name": "Morningstar",
+        "price": "10 sp",
+        "damage": "1d6 B",
+        "bulk": "1",
+        "hands": 1,
+        "group": "Club",
+        "traits": ["Versatile P"]
+    },
+    {
+        "name": "Sickle",
+        "price": "2 sp",
+        "damage": "1d4 S",
+        "bulk": "L",
+        "hands": 1,
+        "group": "Knife",
+        "traits": ["Agile", "finesse", "trip"]
+    },
+    {
+        "name": "Spear",
+        "price": "1 sp",
+        "damage": "1d6 P",
+        "bulk": "1",
+        "hands": 1,
+        "group": "Spear",
+        "traits": ["Thrown 20 ft."]
+    },
+    {
+        "name": "Spiked gauntlet",
+        "price": "3 sp",
+        "damage": "1d4 P",
+        "bulk": "L",
+        "hands": 1,
+        "group": "Brawling",
+        "traits": ["Agile", "free-hand"]
+    },
+    {
+        "name": "Staff",
+        "price": "0 sp",
+        "damage": "1d4 B",
+        "bulk": "1",
+        "hands": 1,
+        "group": "Club",
+        "traits": ["Two-hand d8"]
+    }
+]
+
+MARTIAL_MELEE = [
+    {
+        "name": "Bastard sword",
+        "price": "35 sp",
+        "damage": "1d8 P",
+        "bulk": "1",
+        "hands": 1,
+        "group": "Sword",
+        "traits": ["Two-hand d12"]
+    },
+    {
+        "name": "Battle axe",
+        "price": "12 sp",
+        "damage": "1d8 S",
+        "bulk": "1",
+        "hands": 1,
+        "group": "Axe",
+        "traits": ["Sweep"]
+    },
+    {
+        "name": "Bo staff",
+        "price": "2 sp",
+        "damage": "1d8 B",
+        "bulk": "1",
+        "hands": 2,
+        "group": "Club",
+        "traits": ["Monk", "parry", "reach", "trip"]
+    },
+    {
+        "name": "Falchion",
+        "price": "30 sp",
+        "damage": "1d10 S",
+        "bulk": "1",
+        "hands": 2,
+        "group": "Sword",
+        "traits": ["Forceful", "sweep"]
+    },
+    {
+        "name": "Flail",
+        "price": "8 sp",
+        "damage": "1d6 B",
+        "bulk": "1",
+        "hands": 1,
+        "group": "Flail",
+        "traits": ["Disarm", "sweep", "trip"]
+    },
+    {
+        "name": "Glaive",
+        "price": "10 sp",
+        "damage": "1d8 S",
+        "bulk": "2",
+        "hands": 2,
+        "group": "Polearm",
+        "traits": ["Deadly d8", "forceful", "reach"]
+    },
+    {
+        "name": "Greataxe",
+        "price": "22 sp",
+        "damage": "1d12 S",
+        "bulk": "2",
+        "hands": 2,
+        "group": "Axe",
+        "traits": ["Sweep"]
+    },
+    {
+        "name": "Greatclub",
+        "price": "12 sp",
+        "damage": "1d10 B",
+        "bulk": "2",
+        "hands": 2,
+        "group": "Club",
+        "traits": ["Backswing", "shove"]
+    },
+    {
+        "name": "Greatpick",
+        "price": "14 sp",
+        "damage": "1d10 P",
+        "bulk": "2",
+        "hands": 2,
+        "group": "Pick",
+        "traits": ["Fatal d12"]
+    },
+    {
+        "name": "Greatsword",
+        "price": "20 sp",
+        "damage": "1d12 S",
+        "bulk": "2",
+        "hands": 2,
+        "group": "Sword",
+        "traits": ["Versatile P"]
+    },
+    {
+        "name": "Guisarme",
+        "price": "14 sp",
+        "damage": "1d10 S",
+        "bulk": "2",
+        "hands": 2,
+        "group": "Polearm",
+        "traits": ["Reach", "trip"]
+    },
+    {
+        "name": "Halberd",
+        "price": "18 sp",
+        "damage": "1d10 P",
+        "bulk": "2",
+        "hands": 2,
+        "group": "Polearm",
+        "traits": ["Reach", "versatile S"]
+    },
+    {
+        "name": "Hatchet",
+        "price": "4 sp",
+        "damage": "1d6 S",
+        "bulk": "L",
+        "hands": 1,
+        "group": "Axe",
+        "traits": ["Agile", "sweep", "thrown 10 ft."]
+    },
+    {
+        "name": "Heavy shield bash",
+        "price": "-",
+        "damage": "1d4 B",
+        "bulk": "-",
+        "hands": 1,
+        "group": "Shield",
+        "traits": ["-"]
+    },
+    {
+        "name": "Heavy shield boss",
+        "price": "5 sp",
+        "damage": "1d6 B",
+        "bulk": "-",
+        "hands": 1,
+        "group": "Shield",
+        "traits": ["Attached to heavy shield"]
+    },
+    {
+        "name": "Heavy shield spikes",
+        "price": "5 sp",
+        "damage": "1d6 P",
+        "bulk": "-",
+        "hands": 1,
+        "group": "Shield",
+        "traits": ["Attached to heavy shield"]
+    },
+    {
+        "name": "Lance",
+        "price": "12 sp",
+        "damage": "1d8 P",
+        "bulk": 2,
+        "hands": 2,
+        "group": "Spear",
+        "traits": ["Charge", "deadly d8", "reach"]
+    },
+    {
+        "name": "Light hammer",
+        "price": "3 sp",
+        "damage": "1d6 B",
+        "bulk": "L",
+        "hands": 1,
+        "group": "Hammer",
+        "traits": ["Agile", "thrown 20 ft."]
+    },
+    {
+        "name": "Light pick",
+        "price": "4 sp",
+        "damage": "1d4 P",
+        "bulk": "L",
+        "hands": 1,
+        "group": "Pick",
+        "traits": ["Agile", "fatal d8"]
+    },
+    {
+        "name": "Light shield bash",
+        "price": "-",
+        "damage": "1d3 B",
+        "bulk" : "-",
+        "hands": 1,
+        "group": "Shield",
+        "traits": ["Agile"]
+    },
+    {
+        "name": "Light shield boss",
+        "price": "4 sp",
+        "damage": "1d4 B",
+        "bulk": "-",
+        "hands": 1,
+        "group": "Shield",
+        "traits": ["Agile", "attached to light shield"]
+    },
+    {
+        "name": "Light shield spikes",
+        "price": "4 sp",
+        "damage": "1d4 P",
+        "bulk": "-",
+        "hands": 1,
+        "group": "Shield",
+        "traits": ["Agile", "attached to light shield"]
+    },
+    {
+        "name": "Longsword",
+        "price": "10 sp",
+        "daage": "1d8 S",
+        "bulk": 1,
+        "hands": 1,
+        "group": "Sword",
+        "traits": ["Versatile P"]
+    },
+    {
+        "name": "Main–gauche",
+        "price": "5 sp",
+        "damage": "1d4 P",
+        "bulk": "L",
+        "hands": 1,
+        "group": "Knife",
+        "traits": ["Agile", "disarm", "finesse", "parry", "versatile S"]
+    },
+    {
+        "name": "Maul",
+        "price": "26 sp",
+        "damage": "1d12 B",
+        "bulk": "2",
+        "hands": 2,
+        "group": "Hammer",
+        "traits": ["Shove"]
+    },
+    {
+        "name": "Pick",
+        "price": "7 sp",
+        "damage": "1d6 P",
+        "bulk": 1,
+        "hands": 1,
+        "group": "Pick",
+        "traits": ["Fatal d10"]
+    },
+    {
+        "name": "Ranseur",
+        "price": "14 sp",
+        "damage": "1d10 P",
+        "bulk": 2,
+        "hands": 2,
+        "group": "Polearm",
+        "traits": ["Disarm", "reach"]
+    },
+    {
+        "name": "Rapier",
+        "price": "15 sp",
+        "damage": "1d6 P",
+        "bulk": 1,
+        "hands": 1,
+        "group": "Sword",
+        "traits": ["Deadly d8", "disarm", "finesse"]
+    },
+    {
+        "name": "Sap",
+        "price": "1 sp",
+        "damage": "1d6 B",
+        "bulk": "L",
+        "hands": 1,
+        "group": "Club",
+        "traits": ["Agile", "nonlethal"]
+    },
+    {
+        "name": "Scimitar",
+        "price": "11 sp",
+        "damage": "1d6 S",
+        "bulk": 1,
+        "hands": 1,
+        "group": "Sword",
+        "traits": ["Forceful", "sweep"]
+    },
+    {
+        "name": "Scythe",
+        "price": "18 sp",
+        "damage": "1d10 S",
+        "bulk": 2,
+        "hands": 2,
+        "group": "Polearm",
+        "traits": ["Deadly d10", "trip"]
+    },
+    {
+        "name": "Shortsword",
+        "price": "9 sp",
+        "damage": "1d6 P",
+        "bulk": "L",
+        "hands": 1,
+        "group": "Sword",
+        "traits": ["Agile", "finesse", "versatile S"]
+    },
+    {
+        "name": "Starknife",
+        "price": "24 sp",
+        "damage": "1d4 P",
+        "bulk": "L",
+        "hands": 1,
+        "group": "Knife",
+        "traits": ["Agile", "deadly d6", "finesse", "thrown 20 ft.", "versatile S"]
+    },
+    {
+        "name": "Trident",
+        "price": "13 sp",
+        "damage": "1d8 P",
+        "bulk": 1,
+        "hands": 1,
+        "group": "Spear",
+        "traits": ["Thrown 20 ft."]
+    },
+    {
+        "name": "War flail",
+        "price": "15 sp",
+        "damage": "1d10 B",
+        "bulk": 2,
+        "hands": 2,
+        "group": "Flail",
+        "traits": ["Disarm", "sweep", "trip"]
+    },
+    {
+        "name": "Warhammer",
+        "price": "12 sp",
+        "damage": "1d8 B",
+        "bulk": 1,
+        "hands": 1,
+        "group": "Hammer",
+        "traits": ["Shove"]
+    },
+    {
+        "name": "Whip",
+        "price": "1 sp",
+        "damage": "1d4 S",
+        "bulk": 1,
+        "hands": 1,
+        "group": "Flail",
+        "traits": ["Disarm", "finesse", "nonlethal", "reach", "trip"]
+    }
+]
